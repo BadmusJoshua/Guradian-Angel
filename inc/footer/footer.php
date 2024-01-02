049e27f8779a293aa8a54bc4c5b78b889975cb90
Badmus Joshua <joshuabadnus574@gmail.com>
2024-01-02 12:06:57 +0100

    started working on responsiveness and backend for index

---
+++ inc/footer/footer.php
@@ -0,0 +1,40 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer text-center">
+      <div class="copyright">
+          &copy; Copyright <strong><span>Jobcrest</span></strong>. All Rights Reserved
+      </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template JS Files -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/isotope-3.0.6.min.js"></script>
+  <script src="js/chosen.min.js"></script>
+  <script src="js/moment.min.js"></script>
+  <script src="js/daterangepicker.js"></script>
+  <script src="js/purecounter.js"></script>
+  <script src="js/progresscircle.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+  </body>
+
+  </html>
